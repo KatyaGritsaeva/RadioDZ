4b379e642d94bd347a1167ea141cd736dbcbe942
KatyaGritsaeva <gekaterina2019@gmail.com>
2022-06-17 23:56:41 +0300

    add test commit

--- src/main/java/com/gritsaeva/RadioMain.java
+++ src/main/java/com/gritsaeva/RadioMain.java
@@ -1,20 +1,65 @@
 package com.gritsaeva;
 
 public class RadioMain {
-// currentVolume;
+    public int currentVolume;
+    public int currentstation;
+
     public void increaseVolume() {
-        int currentVolume = 0;
         if (currentVolume < 10) {
             currentVolume = currentVolume + 1;
         }
-        else currentVolume = currentVolume;
     }
 
-    public void RadioStationNumber() {
-        int currentstation = 0;
-        if (currentstation < 9) {
-            currentstation = currentstation + 1;
+    public void lessMinVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
         }
-        else currentstation = currentstation;
     }
+
+    public void nextStation() {
+        if (currentstation < 9) {
+            currentstation++;
+        } else {
+            currentstation = 0;
+        }
+    }
+
+
+    public void prewStation() {
+        if (currentstation > 0) {
+            currentstation--;
+        } else {
+            currentstation = 9;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentstation() {
+        return currentstation;
+    }
+
+    public void setCurrentstation(int currentstation) {
+        if (currentstation < 0) {
+            return;
+        }
+        if (currentstation > 9) {
+            return;
+        }
+        this.currentstation = currentstation;
+    }
+
 }
+
--- src/test/java/com/gritsaeva/RadioMainTest.java
+++ src/test/java/com/gritsaeva/RadioMainTest.java
@@ -1,13 +1,164 @@
 package com.gritsaeva;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RadioMainTest {
 
     @Test
-    public void RadioMainTest(){
-        RadioMain service = new RadioMain();
+    public void RadioMainNextStationBoundaryMaxTest() {
+        RadioMain station = new RadioMain();
+        station.setCurrentstation(9);
+
+        station.nextStation();
+
+        int expected = 0;
+        int actual = station.getCurrentstation();
+        Assertions.assertEquals(expected, actual);
+
     }
 
+    @Test
+    public void RadioMainPrewStationBoundaryMinTest() {
+        RadioMain station = new RadioMain();
+        station.setCurrentstation(0);
+
+        station.prewStation();
+
+        int expected = 9;
+        int actual = station.getCurrentstation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainPrewStationBoundaryMaxTest() {
+        RadioMain station = new RadioMain();
+        station.setCurrentstation(10);
+
+        station.prewStation();
+
+        int expected = 9;
+        int actual = station.getCurrentstation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainPrewStationBoundaryTest() {
+        RadioMain station = new RadioMain();
+        station.setCurrentstation(5);
+
+        station.prewStation();
+
+        int expected = 4;
+        int actual = station.getCurrentstation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainNextStationBoundaryMinTest() {
+        RadioMain station = new RadioMain();
+        station.setCurrentstation(0);
+
+        station.nextStation();
+
+        int expected = 1;
+        int actual = station.getCurrentstation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainNextStationTest() {
+        RadioMain station = new RadioMain();
+        station.setCurrentstation(4);
+
+        station.nextStation();
+
+        int expected = 5;
+        int actual = station.getCurrentstation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainIncreaseVolumeTest() {
+        RadioMain volume = new RadioMain();
+        volume.setCurrentVolume(5);
+
+        volume.increaseVolume();
+
+        int expected = 6;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainIncreaseBoundaryMaxVolumeTest() {
+        RadioMain volume = new RadioMain();
+        volume.setCurrentVolume(10);
+
+        volume.increaseVolume();
+
+        int expected = 10;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainIncreaseBoundaryMinVolumeTest() {
+        RadioMain volume = new RadioMain();
+        volume.setCurrentVolume(0);
+
+        volume.increaseVolume();
+
+        int expected = 1;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainlessMinBoundaryMinVolumeTest() {
+        RadioMain volume = new RadioMain();
+        volume.setCurrentVolume(0);
+
+        volume.lessMinVolume();
+
+        int expected = 0;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainlessMinBoundaryMaxVolumeTest() {
+        RadioMain volume = new RadioMain();
+        volume.setCurrentVolume(10);
+
+        volume.lessMinVolume();
+
+        int expected = 9;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void RadioMainlessMinMiddleVolumeTest() {
+        RadioMain volume = new RadioMain();
+        volume.setCurrentVolume(6);
+
+        volume.lessMinVolume();
+
+        int expected = 5;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
 
 }
